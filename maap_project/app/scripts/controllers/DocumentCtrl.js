d0d03d8b11cc9c2206433eede470d41ff9d74819
Jack <giacomo.pinato@gmail.com>
2014-08-13 00:09:38 +0200

    Nested document working, only 1 level of inception

--- maap_project/app/scripts/controllers/DocumentCtrl.js
+++ maap_project/app/scripts/controllers/DocumentCtrl.js
@@ -18,6 +18,28 @@ angular.module('maaperture').controller('DocumentCtrl', function ($scope, $locat
         return typeof temp === 'object';
     }
 
+    $scope.parseResponse = function (data, index) {
+        var values=[];
+        if (index > 1) {
+            $.each(data, function (key, value) {
+
+                if ($scope.isObject(value)) {
+                     var innerObject = $scope.parseResponse(value,index-1);
+                     values.push(innerObject);
+                }
+                //salva i valori in un array per non perdere l'ordinamento
+
+                else {
+                     values.push(value);
+                }
+            });
+
+            return values;
+        }
+        else{
+            return values;
+        }
+    };
     //Funzione per richiedere un documento al server.
     //Passa come parametri la collection e il documento da ricevere
 
@@ -29,24 +51,11 @@ angular.module('maaperture').controller('DocumentCtrl', function ($scope, $locat
             $scope.originalJson =  JSON.stringify(response.data,undefined, 2); // indentation level = 2
             $scope.data = $scope.tempData;
 
-            $.each($scope.data, function (key, value) {
-
-                if ($scope.isObject(value)){
-                    $scope.innerObject = [];
-                    $.each(value, function (key, value) {
-                        $scope.innerObject.push(value);
-                    });
-                    $scope.values.push($scope.innerObject);
-                }
-                //salva i valori in un array per non perdere l'ordinamento
-
-                else {
-                    $scope.values.push(value);
-                }
-            });
+            $scope.values = $scope.parseResponse($scope.data,3);
             //Salva le etichette in un array
             //$scope.labels = response.label;
-            $scope.labels = ["nome","cognome","email","eta","interessi","bitches"];
+            $scope.labels = ["nome","cognome","email","eta",["interessi","uno: ","due: "],["bitches","Mora: ","Bionda: ","Rossa: "]];
+
 
         },
 
@@ -74,63 +83,3 @@ angular.module('maaperture').controller('DocumentCtrl', function ($scope, $locat
 
 });
 
-
-/*
-* OLD CONTROLLER
-*
-* 'use strict';
-
- angular.module('maaperture').controller('DocumentCtrl', function ($scope, $location, DocumentDataService, DocumentEditService, $routeParams, $cookieStore) {
-
- $scope.current_collection = $routeParams.col_id;
- $scope.current_document = $routeParams.doc_id;
- $scope.values = [];
- $scope.canEdit = $cookieStore.get("isAdmin");
-
- //Funzione per richiedere un documento al server.
- //Passa come parametri la collection e il documento da ricevere
-
- DocumentDataService.query({
- col_id: $routeParams.col_id,
- doc_id: $routeParams.doc_id }).$promise.then(
-
- function success(response) {
- $scope.originalJson =  JSON.stringify(response.data,undefined, 2); // indentation level = 2
- $scope.data = response.data;
- $.each($scope.data, function (key, value) {
- //salva i valori in un array per non perdere l'ordinamento
-
- $scope.values.push(value);
-
- });
- //Salva le etichette in un array
- $scope.labels = response.label;
- },
-
- function error() {
- $location.path("/" + $scope.current_collection);
- }
-
- );
-
- //Funzione per cancellare il documento attualmente visualizzato
- $scope.delete_document = function () {
- DocumentEditService.remove({
- col_id: $scope.current_collection,
- doc_id: $scope.current_document
- }).$promise.then(function success() {
- $location.path('/collection/' + $scope.current_collection);
-
- },
- function err() {
- alert("Something went wrong, document could not be deleted");
- }
- );
- };
-
- });
-
- *
-*
-*
-* */
